2a4c3f41719d6c7854cecc942c657554578f2956
Abel-Tom <icecakeinc@gmail.com>
2024-08-06 23:44:13 +0530

    removed cron jobs

--- api/cron.ts
+++
@@ -1,9 +0,0 @@
-import express, { Express, Request, Response } from 'express';
-import { cronTask } from "./index"
-
-
-export const CronJob = async (req: Request,res:Response) =>{
-    console.log('cron')
-    cronTask()
-    res.status(200).json({"Greetings Stranger! ": "We are Express and TypeScript Added  some extra features"});
-}
