4060de71eaebb3f3c82664853eb4f25bc47eeede
Abel-Tom <icecakeinc@gmail.com>
2024-08-06 23:15:56 +0530

    added cron jobs

--- api/index.ts
+++ api/index.ts
@@ -36,10 +36,10 @@ cron.schedule(getCronExpression(), async () => {
   inMemDb = await CheckMinutesAndSendMail(inMemDb);
 });
 
-// export const cronTask =  async (): Promise<void> => {
-//   console.log('started task')
-//   inMemDb = await CheckMinutesAndSendMail(inMemDb);
-// };
+export const cronTask =  async (): Promise<void> => {
+  console.log('started task')
+  inMemDb = await CheckMinutesAndSendMail(inMemDb);
+};
 
 app.listen(SERVER_PORT, () => {
     console.log(`${NODE_ENV} server listening on port ${SERVER_PORT}.`);
